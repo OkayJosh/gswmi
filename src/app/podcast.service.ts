9fbd3da6222930673c89ade355578a1bd3bb5671
Omonbude Emmanuel <gospeltruth18@gmail.com>
2020-07-09 09:31:48 +0100

    podcast added

--- src/app/podcast.service.ts
+++ src/app/podcast.service.ts
@@ -5,15 +5,21 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class PodcastService {
-  getpodcasturl = 'http://theblogapi.herokuapp.com/posts/';
+  getposturl = 'http://theblogapi.herokuapp.com/posts/';
+  getpodcasturl = 'https://api.spreaker.com/v2/users/11845707/episodes'
   constructor(private http: HttpClient) { }
   allpost() {
     // const key = localStorage.getItem('apikey');
-       return this.http.get<any>(this.getpodcasturl );
+       return this.http.get<any>(this.getposturl );
  }
   getpost(id) {
     // const key = localStorage.getItem('apikey');
-       return this.http.get<any>(this.getpodcasturl + id + '/' );
+       return this.http.get<any>(this.getposturl + id + '/' );
  }
-
+ getPodcasts(){
+  return this.http.get<any>(this.getpodcasturl );
+ }
+ getNextPodcast(url){
+  return this.http.get<any>(url);
+ }
 }
--- src/app/podcast/podcast.component.ts
+++ src/app/podcast/podcast.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PodcastService } from '../podcast.service';
 
 @Component({
   selector: 'app-podcast',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./podcast.component.scss']
 })
 export class PodcastComponent implements OnInit {
+  podcastlist = [];
+  nexturl = '';
+  constructor(private api:PodcastService) { }
+getPodcast(){
+  this.api.getPodcasts().subscribe(
+    data => {
+      this.podcastlist = data['response']['items'];
+      this.nexturl = data["response"]["next_url"];
+      console.log(data);
+    },
+    error => {
+      console.log(error.error);
+    }
+  );
+}
+getNext(){
+this.api.getNextPodcast(this.nexturl).subscribe(
+  data => {
+    console.log(data['response']['items']);
+    this.podcastlist = data['response']['items'];
+    if (data["response"]["next_url"] == null){
+      this.nexturl = '';
+    }else {
+      this.nexturl = data["response"]["next_url"];
+    }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  },
+  error => {
+    console.log(error.error);
+  }
+);
+}
+ngOnInit(): void {
+    this.getPodcast();
   }
 
 }
