703b9d596a6d1915c341441895998f8504cbe3b9
okayjosh <19627177+OkayJosh@users.noreply.github.com>
2020-07-10 11:23:42 +0100

    fixing ckeditor path error

--- src/assets/js/ckeditor/ckeditor-config.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    CKEDITOR.basePath = '/assets/js/ckeditor/'
-    CKEDITOR.plugins.addExternal('wordcount', 'plugins/wordcount/');
-    CKEDITOR.plugins.addExternal('notification', 'plugins/notification/');
-    CKEDITOR.editorConfig = function( config ) {
-        config.extraPlugins = 'wordcount,notification';
-    }
-})();
