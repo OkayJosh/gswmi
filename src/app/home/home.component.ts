f43766501264c5a18fa73c7cb22e343e86311529
okayjosh <19627177+OkayJosh@users.noreply.github.com>
2020-07-16 10:00:44 +0100

    fix for dasboard

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap'
-import { Chart } from 'chart.js';
+
 
 @Component({
   selector: 'app-home',
@@ -8,129 +7,13 @@ import { Chart } from 'chart.js';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  closeResult = '';
 
-  constructor(private modalService: NgbModal) { }
+  constructor() { }
 
-  open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
 
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
-  }
-  
   ngOnInit() {
     const body = document.getElementsByTagName('body')[0];
     body.classList.add('landing-page');
-
-    const canvas: any = document.getElementById('chartBig');
-    const ctx = canvas.getContext('2d');
-    const gradientFill = ctx.createLinearGradient(0, 350, 0, 50);
-    gradientFill.addColorStop(0, 'rgba(228, 76, 196, 0.0)');
-    gradientFill.addColorStop(1, 'rgba(228, 76, 196, 0.14)');
-    const chartBig = new Chart(ctx, {
-      type: 'line',
-      responsive: true,
-      data: {
-        labels: [
-          'JUN',
-          'FEB',
-          'MAR',
-          'APR',
-          'MAY',
-          'JUN',
-          'JUL',
-          'AUG',
-          'SEP',
-          'OCT',
-          'NOV',
-          'DEC'
-        ],
-        datasets: [
-          {
-            label: 'Data',
-            fill: true,
-            backgroundColor: gradientFill,
-            borderColor: '#e44cc4',
-            borderWidth: 2,
-            borderDash: [],
-            borderDashOffset: 0.0,
-            pointBackgroundColor: '#e44cc4',
-            pointBorderColor: 'rgba(255,255,255,0)',
-            pointHoverBackgroundColor: '#be55ed',
-            //pointHoverBorderColor:'rgba(35,46,55,1)',
-            pointBorderWidth: 20,
-            pointHoverRadius: 4,
-            pointHoverBorderWidth: 15,
-            pointRadius: 4,
-            data: [80, 160, 200, 160, 250, 280, 220, 190, 200, 250, 290, 320]
-          }
-        ]
-      },
-      options: {
-        maintainAspectRatio: false,
-        legend: {
-          display: false
-        },
-
-        tooltips: {
-          backgroundColor: '#fff',
-          titleFontColor: '#ccc',
-          bodyFontColor: '#666',
-          bodySpacing: 4,
-          xPadding: 12,
-          mode: 'nearest',
-          intersect: 0,
-          position: 'nearest'
-        },
-        responsive: true,
-        scales: {
-          yAxes: [
-            {
-              barPercentage: 1.6,
-              gridLines: {
-                drawBorder: false,
-                color: 'rgba(0,0,0,0.0)',
-                zeroLineColor: 'transparent'
-              },
-              ticks: {
-                display: false,
-                suggestedMin: 0,
-                suggestedMax: 350,
-                padding: 20,
-                fontColor: '#9a9a9a'
-              }
-            }
-          ],
-
-          xAxes: [
-            {
-              barPercentage: 1.6,
-              gridLines: {
-                drawBorder: false,
-                color: 'rgba(0,0,0,0)',
-                zeroLineColor: 'transparent'
-              },
-              ticks: {
-                padding: 20,
-                fontColor: '#9a9a9a'
-              }
-            }
-          ]
-        }
-      }
-    });
   }
 
   onwhatsappclick(){
