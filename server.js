f6ad9740a2cd32d14e3c969c716a97e23a6191f3
Olatunji Joshua Kayode <19627177+OkayJosh@users.noreply.github.com>
2020-10-02 00:18:38 +0100

    removed https

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const path = require('path');
 const app = express();
 
 
-if(process.env.NODE_ENV === 'production') {
-    app.use((req, res, next) => {
-      if (req.header('x-forwarded-proto') !== 'https')
-        res.redirect(`https://${req.header('host')}${req.url}`)
-      else
-        next()
-    })
-  }
+//if(process.env.NODE_ENV === 'production') {
+//    app.use((req, res, next) => {
+//      if (req.header('x-forwarded-proto') !== 'https')
+//        res.redirect(`https://${req.header('host')}${req.url}`)
+//      else
+//        next()
+//    })
+//  }
 
 // fix to use compression
 app.use(compression());
