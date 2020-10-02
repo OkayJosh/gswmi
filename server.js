1233299639011fe3d0711acfd81f2fda0014b8a2
Olatunji Joshua Kayode <19627177+OkayJosh@users.noreply.github.com>
2020-10-02 01:08:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ const path = require('path');
 const app = express();
 
 
-if(process.env.NODE_ENV === 'production') {
-    app.use((req, res, next) => {
-      if (req.header('x-forwarded-proto') !== 'https')
+//if(process.env.NODE_ENV === 'production') {
+//    app.use((req, res, next) => {
+//      if (req.header('x-forwarded-proto') !== 'https')
         //res.redirect(`https://${req.header('host')}${req.url}`)
-          res.redirect(`http://${req.header('host')}${req.url}`)
-      else
-        next()
-    })
-}
+//          res.redirect(`http://${req.header('host')}${req.url}`)
+//      else
+//        next()
+//    })
+//}
 
 // fix to use compression
 app.use(compression());
